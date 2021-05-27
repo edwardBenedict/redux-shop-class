d6b04e08085918030ee6ba3a235db2fda9261286
ed ben <edwardbenedict92@gmail.com>
2021-05-27 21:49:49 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import LaptopComp from "./components/LaptopComp";
+import PhoneComp from "./components/PhoneComp";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Clarus Shop</h2>
+      <LaptopComp />
+      <PhoneComp />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/redux/actions/actions.js
@@ -0,0 +1,25 @@
+import { SELL_LAPTOP, SELL_COMP, SELL_WATCH, SELL_PHONE } from "../types/types";
+
+export const sellLaptop = () => {
+  return {
+    type: SELL_LAPTOP,
+  };
+};
+
+export const sellPhone = () => {
+  return {
+    type: SELL_PHONE,
+  };
+};
+
+export const sellWatch = () => {
+  return {
+    type: SELL_WATCH,
+  };
+};
+
+export const sellComp = () => {
+  return {
+    type: SELL_COMP,
+  };
+};
